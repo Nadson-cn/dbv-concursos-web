92e49a7b63f600444276383d60cc4048ecfff1ab
Nadson Cardoso <contato.nadson10@gmail.com>
2023-08-17 22:41:03 -0300

    remove login page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,8 +8,8 @@ const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="admin/login" replace />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/" element={<Navigate to="admin/login" replace />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/result" element={<ResultsScreen />} />
       </Routes>
